7a97eca9554f8e41e5f6203ea9fe3019986371c9
vasukiH <Vasuki.Harichandrapandian1@centurylink.com>
2020-08-21 12:42:47 +0000

    Lab 2

--- main.js
+++ main.js
@@ -96,6 +96,65 @@ function post(serviceNowTable, callback) {
 
 }
 
+/**
+ * @function get
+ * @description Call the ServiceNow POST API..
+ *
+ * @param {string} serviceNowTable - The table target of the ServiceNow table API.
+ * @param {iapCallback} callback - Callback a function.
+ * @param {*} callback.data - The API's response. Will be an object if sunnyday path.
+ *   Will be HTML text if hibernating instance.
+ * @param {error} callback.error - The error property of callback.
+ */
+function get(serviceNowTable, callback) {
+
+  // Initialize return arguments for callback
+  let callbackData = null;
+  let callbackError = null;
+
+  // Construct API call to send to ServiceNow.
+  // The request constructor has an options parameter
+  // that holds the HTTP request method, credentials, and the API's URL.
+  // Some properties are hardcoded, like the method and part of the URI.
+  // Some properties are read from global const options.
+  // Some properties are passed into function get() through parameters.
+  const requestOptions = {
+    method: 'GET',
+    auth: {
+      user: options.username,
+      pass: options.password,
+    },
+    baseUrl: options.url,
+    uri: `/api/now/table/${serviceNowTable}`,
+  };
+
+  // Send Request to ServiceNow.
+  // We are passing variable requestOptions for the first argument.
+  // We are passing an anonymous function, an error-first callback,
+  // for the second argument.
+  request(requestOptions, (error, response, body) => {
+    /**
+     * Process ServiceNow error, response and body.
+     * Check error and response code to make sure
+     * response is good.
+     */
+    if (error) {
+      console.error('Error present.');
+      callbackError = error;
+    } else if (!validResponseRegex.test(response.statusCode)) {
+      console.error('Bad response code.');
+      callbackError = response;
+    } else if (response.body.includes('Instance Hibernating page')) {
+      callbackError = 'Service Now instance is hibernating';
+      console.error(callbackError);
+    } else {
+      callbackData = response;
+    }
+    return callback(callbackData, callbackError);
+  });
+
+}
+
 
 /*
  * This section is used to test your project.
