f12d92ac880305e781c236a581bf154a93b751e0
vasukiH <Vasuki.Harichandrapandian1@centurylink.com>
2020-08-29 09:17:16 +0000

    Final Project

--- main.js
+++ main.js
@@ -131,6 +131,44 @@ healthcheck(callback) {
       log.debug(`\nId:\n${JSON.stringify(this.id)} \n message :\n${JSON.stringify(result)}`);
    }
  });
+
+ this.postRecord((result, error) => {
+   /**
+    * For this lab, complete the if else conditional
+    * statements that check if an error exists
+    * or the instance was hibernating. You must write
+    * the blocks for each branch.
+    */
+   if (error) {
+     /**
+      * Write this block.
+      * If an error was returned, we need to emit OFFLINE.
+      * Log the returned error using IAP's global log object
+      * at an error severity. In the log message, record
+      * this.id so an administrator will know which ServiceNow
+      * adapter instance wrote the log message in case more
+      * than one instance is configured.
+      * If an optional IAP callback function was passed to
+      * healthcheck(), execute it passing the error seen as an argument
+      * for the callback's errorMessage parameter.
+      */
+      this.emitOffline();
+      log.error(`\nId:\n${JSON.stringify(this.id)} \nerror :\n${JSON.stringify(error)}`);
+   } else {
+     /**
+      * Write this block.
+      * If no runtime problems were detected, emit ONLINE.
+      * Log an appropriate message using IAP's global log object
+      * at a debug severity.
+      * If an optional IAP callback function was passed to
+      * healthcheck(), execute it passing this function's result
+      * parameter as an argument for the callback function's
+      * responseData parameter.
+      */
+       this.emitOnline();
+      log.debug(`\nId:\n${JSON.stringify(this.id)} \n message :\n${JSON.stringify(result)}`);
+   }
+ });
 }
 
   /**
@@ -195,6 +233,7 @@ healthcheck(callback) {
             work_end:null,
             change_ticket_key:null
         };
+        
 
      this.connector.get((res, err) => {
           if(res.body){
@@ -208,6 +247,7 @@ healthcheck(callback) {
             changeticket.work_start=response.result[i].work_start;
             changeticket.work_end=response.result[i].work_end;
             changeticket.change_ticket_key=response.result[i].sys_id;
+            //changetickets.push(changeticket);
             }
           }
 
